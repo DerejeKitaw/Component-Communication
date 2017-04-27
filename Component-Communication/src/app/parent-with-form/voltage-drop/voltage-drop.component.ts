75d59dd96c79bf10fa4c24b2544e167e4a8ba26f
Dereje Kitaw <dereje.kitaw@gmail.com>
2017-04-26 22:18:25 -0400

    factorize voltage drop component

--- Component-Communication/src/app/parent-with-form/voltage-drop/voltage-drop.component.ts
+++ Component-Communication/src/app/parent-with-form/voltage-drop/voltage-drop.component.ts
@@ -6,14 +6,47 @@ import { Component, OnInit,Input } from '@angular/core';
   styleUrls: ['./voltage-drop.component.css']
 })
 export class VoltageDropComponent implements OnInit {
+private _ohm_kft;
+private _current=0;
+private _distance=0;
+private _voltage=0;
+private _vd;
+private voltageDrop ;
+distanceComment: string='<>';
 @Input() wireData;
-@Input() current;
-@Input() distance;
-@Input() voltage;
+
+@Input() 
+set current (current: number) {
+    this._current = current ;
+    this.update();
+  }
+  get current(): number{return this._current;}
+@Input() 
+  set distance (distance: number) {
+    // if (distance > 100){
+    //   this.update();
+    //   this._distance =  10;
+    //   this.distanceComment='too long'
+    // }else{
+    //   this.distanceComment='short'
+
+    // }
+    this._distance = distance ;
+    this.update();
+  }
+  get distance(): number{return this._distance;}
+@Input() 
+set voltage (voltage: number) {
+    this._voltage = voltage ;
+    this.update();
+  }
+  get voltage(): number{return this._voltage;}
 @Input() vd;
   constructor() { }
 
   ngOnInit() {
   }
-
+update(){
+this.voltageDrop = this._current * 2 * this._distance / 1000
+}
 }
